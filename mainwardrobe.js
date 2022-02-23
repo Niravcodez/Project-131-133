8747b92b93ea373607c2280e06a620bd73207d84
Niravcodez <84302842+Niravcodez@users.noreply.github.com>
2022-02-22 17:33:41 -0800

    Add files via upload

--- mainwardrobe.js
+++ mainwardrobe.js
@@ -1,4 +1,5 @@
 img = "";
+status = "";
 
 function preload()
 {
@@ -9,6 +10,8 @@ function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function draw()
@@ -16,3 +19,18 @@ function draw()
     image(img, 0, 0, 640, 420);
 }
 
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
